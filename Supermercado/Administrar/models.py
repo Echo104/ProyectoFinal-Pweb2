a2fb61d062b14867fbfff44fbde20874f528f0b2
Kazel 10 <echoquequi@unsa.edu.pe>
2024-12-10 08:49:14 -0500

    Creacion de los modelos

--- Supermercado/Administrar/models.py
+++ Supermercado/Administrar/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+class Producto(models.Model):
+    nombre=models.CharField(max_length=250)
+    precio_compra = models.DecimalField(max_digits=10, decimal_places=2)
+    precio_venta = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    proveedor = models.ForeignKey('Proveedor', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
+    
+class Proveedor(models.Model):
+    nombre = models.CharField(max_length=255)
+    direccion = models.CharField(max_length=255)
+    telefono = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.nombre
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nombre
+
+class Venta(models.Model):
+    cliente = models.ForeignKey('Cliente', on_delete=models.CASCADE, null=True)
+    fecha = models.DateTimeField(auto_now_add=True)
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Venta {self.id} - {self.fecha}"
